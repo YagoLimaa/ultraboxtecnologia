47a8a88826980d10deb2a24bdbd7fb2e75b46baa
Yago <yagolazevedo@gmail.com>
2026-01-15 15:58:06 -0300

    removi teste

--- functions/api/create-payment.js
+++ functions/api/create-payment.js
@@ -3,7 +3,7 @@ const formatTLV = (id, value) => {
   return `${id}${len}${value}`;
 };
 
-// CRC16-CCITT calculation used in PIX QR codes teste
+// CRC16-CCITT calculation used in PIX QR codes 
 const crc16 = (data) => {
   let crc = 0xFFFF;
   for (let i = 0; i < data.length; i++) {
